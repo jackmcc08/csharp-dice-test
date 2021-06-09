7dbbcfad76f4162f9633081bcc8eedd7be6ab8f3
jackmcc08 <jackmcc08@gmail.com>
2021-06-09 17:47:48 +0100

    initial creation of simpleDice program

---
+++ simpleDice/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace simpleDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(WelcomeJungle());
+        }
+
+        static string WelcomeJungle()
+        {
+            return "Welcome to the Jungle!";
+        }
+    }
+}
---
+++ simpleDiceTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using simpleDice;
+
+
+namespace simpleDiceTest
+{
+    [TestClass]
+    public class simpleTests
+    {
+        [TestMethod]
+        public void WelcomeJungleTest()
+        {
+
+        }
+    }
+}
