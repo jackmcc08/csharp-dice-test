a980b5f900ff3824151fbf0f19ab7447eeca05ec
jackmcc08 <jackmcc08@gmail.com>
2021-06-10 13:26:14 +0100

    created test for rolling multiple dice and implemented

--- simpleDice/Program.cs
+++ simpleDice/Program.cs
@@ -2,17 +2,70 @@
 
 namespace simpleDice
 {
-    class Program
+    public class Dice
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
             Console.WriteLine(WelcomeJungle());
+
+            int numDice = 0;
+            do
+            {
+                Console.Write("How many dice would you like to roll: ");
+                string userInput = Console.ReadLine();
+                int userInputInt;
+
+                try
+                {
+                    userInputInt = Convert.ToInt32(userInput);
+                } catch 
+                {
+                    Console.WriteLine("Please enter a positive whole number above 0. e.g. 4");
+                    continue;
+                }
+
+                numDice = userInputInt;                    
+            } while (numDice <= 0);
+
+            Console.WriteLine($"You are going to roll {numDice} dice.");
+
+            int[] resultRolls = RollDice(numDice);
+
+            Console.WriteLine("You rolled:");
+            foreach (int roll in resultRolls)
+            {
+                Console.Write($"{roll}, ");
+            }
+
+
+            //Console.WriteLine(Roll());
+            //Array.ForEach(RollDice(2), Console.WriteLine);
+            //Array.ForEach(RollDice(2, true), Console.WriteLine);
+            //Console.WriteLine(RollDice(2));
+            //Console.WriteLine(RollDice(2, true));
         }
 
-        static string WelcomeJungle()
+        public static string WelcomeJungle()
         {
             return "Welcome to the Jungle!";
         }
+
+        public static int Roll(bool test = false)
+        {
+            Random rnd = test ? new Random(1) : new Random();
+            return rnd.Next(1,6);
+        }
+
+        public static int[] RollDice(int numDice, bool test = false)
+        {
+            int[] results = new int[numDice];
+            for (int i = 0; i < numDice; i++)
+            {
+                results[i] = Roll(test);
+            }
+            return results;
+        }
     }
 }
+
--- simpleDiceTest/UnitTest1.cs
+++ simpleDiceTest/UnitTest1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using simpleDice;
 
@@ -11,7 +11,41 @@ namespace simpleDiceTest
         [TestMethod]
         public void WelcomeJungleTest()
         {
+            // Dice test = new Dice();
+            string expected = "Welcome to the Jungle!";
+            string actual = Dice.WelcomeJungle();
+            // Console.WriteLine("Program welcomes you to the Jungle");
+            Assert.AreEqual(expected, actual, false, "This is not connecting");
+        }
 
+        [TestMethod]
+        public void RollingDieGivesNum1to6()
+        {
+            // Arrange
+            int[] possibleResults = {1,2,3,4,5,6};
+
+            // Act
+            int rollResult = Dice.Roll();
+            bool rollBetween1to6 = possibleResults.Contains(rollResult);
+            // Assert
+            Assert.IsTrue(rollBetween1to6, "Roll function is not returning a result between 1 and 6");
+        }
+
+        [TestMethod]
+        public void CanRollAnyNumberOfDie_RollTwoDie()
+        {
+            // Arrange
+            int[] expected = new int[] { 2, 2 };
+            int expectedLength = expected.Length;
+
+
+            // Act
+            int[] actual = Dice.RollDice(2, true);
+            int actualLength = actual.Length;
+
+            // Assert
+            Assert.AreEqual(expectedLength, actualLength, "You did not throw two dice");
+            CollectionAssert.AreEqual(expected, actual, "The thrown dice are not equal");
         }
     }
 }
